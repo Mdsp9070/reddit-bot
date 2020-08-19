112189b507ba0996183010ec5bc411d16ced84d5
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-08-19 11:14:34 -0300

    added CORS and started web application

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, send_from_directory
 from word_count import word_count
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r'/api/*': {'origins': '*'}})
 
 
-@app.route('/words')
+@app.route('/api/words')
 def index():
     body = request.get_json()
     word_count(body['url'], body['filter'], body['uppercase'])
     return {'message': 'successfully counted words!'}
 
 
-@app.route('/files')
+@app.route('/api/files')
 def get_CSV():
     return send_from_directory('..', 'words_count.csv', as_attachment=True)
 
