3fd7a7152053122a8543f736eeebd46efda8d93f
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-08-19 12:04:53 -0300

    added requirements.txt and basic structure web app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [url, setUrl] = useState('');
+  const [uppercase, setUppercase] = useState(false);
+
+  async function handleSubmit() {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={handleSubmit}>
+        <header>
+          <h1>Reddit Word Count Bot!</h1>
+        </header>
+        <main>
+          <input
+            name="url"
+            required
+            onChange={({ target }) => setUrl(target.value)}
+          />
+          <button type="submit">Generate CSV</button>
+          <div id="options">
+            <label>
+              Uppercase Only
+              <input
+                type="checkbox"
+                name="uppercase"
+                onChange={({ target }) => setUppercase(target.checked)}
+              />
+            </label>
+            <br />
+            <label>
+              Upload a CSV containing words to not include
+              <input type="file" name="filter" accept=".csv" />
+            </label>
+          </div>
+        </main>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
